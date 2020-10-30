a7b565ca9cb6d9f2b499abc31c2e0ada5126a490
Jonathan Haines <jonno.haines@gmail.com>
2020-10-30 19:52:55 +1100

    Introduce TypeScript

--- lib/get-package-path.ts
+++ lib/get-package-path.ts
@@ -1,12 +1,12 @@
 /** @babel */
+import type {JsonObject} from 'type-fest';
 import path from 'path';
 import loadJsonFile from 'load-json-file';
 import pkgDir from 'pkg-dir';
 
 const DEFAULT_PACKAGE = {xo: false};
 
-// (base: string) => pkgPath: Promise<string>
-async function findPackage(base) {
+async function findPackage(base: string): Promise<string> {
 	const pkgPath = path.join(base, 'package.json');
 	const pkg = await loadPkg(pkgPath);
 
@@ -17,8 +17,7 @@ async function findPackage(base) {
 	return pkgPath;
 }
 
-// (file: string) => pkg: Promise<Object>
-async function loadPkg(file) {
+async function loadPkg(file: string): Promise<JsonObject> {
 	try {
 		return await loadJsonFile(file);
 	} catch {
@@ -26,8 +25,7 @@ async function loadPkg(file) {
 	}
 }
 
-// (base: string) => pkgPath: Promise<string>
-export default async function getPackagePath(base) {
+export default async function getPackagePath(base: string): Promise<string | null> {
 	const resolvedBase = await pkgDir(base);
 	return resolvedBase ? findPackage(resolvedBase) : null;
 }
