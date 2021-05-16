6f94cde8d051b136b576e7ecc173f5404a0eaaa3
Jonathan Haines <jonno.haines@gmail.com>
2021-05-16 18:40:09 +1000

    Add worker tests

---
+++ mocks/mock-task.js
@@ -0,0 +1,47 @@
+const {Emitter} = require('event-kit');
+
+const privateSpace = new WeakMap();
+
+module.exports.processMessages = function (task) {
+	const {emitter, messages} = privateSpace.get(task);
+
+	for (const {id, ...message} of messages) {
+		emitter.emit(id, message);
+	}
+};
+
+module.exports.MockTask = class MockTask {
+	constructor() {
+		const messages = [];
+		privateSpace.set(this, {
+			childProcess: {connected: false},
+			emitter: new Emitter(),
+			messages
+		});
+	}
+
+	get childProcess() {
+		const {childProcess} = privateSpace.get(this);
+		return {...childProcess};
+	}
+
+	on(event, callback) {
+		const {emitter} = privateSpace.get(this);
+		return emitter.on(event, callback);
+	}
+
+	send(message) {
+		const {messages} = privateSpace.get(this);
+		return messages.push(message);
+	}
+
+	start() {
+		const {childProcess} = privateSpace.get(this);
+		childProcess.connected = true;
+	}
+
+	terminate() {
+		const {childProcess} = privateSpace.get(this);
+		childProcess.connected = false;
+	}
+};
